b39f37f35b137144acce724eba5d88d7acd3186d
Bada2024 <830561@mystma.org>
2021-10-04 22:01:16 -0500

    2nd

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #WRITE YOUR CODE IN THIS FILE
-def hasL(w):
-    for i in range(0,len(w)):
-        if w[i] == "l":
-            return True
-        return False
-
-print(hasL("alabama"))
+def hasL(x):
+    b=list(x)
+    # get a list version of variable
+    for i in range(0,len(b)):
+    #if i (current number in the range) for each i in range of len b
+        if b[i]=="l":
+            #if the list of the array at the postion of i which is the number of times the function has looped around check if the is a letter l
+                return True
+                #if there is a l return true to the terminal
+    return False 
+#if no l is found in the variable b return false.
+print(hasL("floating"))
+        
